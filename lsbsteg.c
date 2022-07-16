a3349f7480df037fdb2999173d0cb64af735aed9
SahiniR <reddysahinip@gmail.com>
2022-07-16 22:12:40 +0530

    opens bmp & accepts string

---
+++ lsbsteg.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+char string[100];
+FILE *bmp;
+bmp=fopen("testimage.bmp","rb");
+printf("please enter the string to be encoded");
+scanf("%s",string);
+}
