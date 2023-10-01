ae75574b09798a6423f5e2ed228fee54c0cd9ba3
SahiniR <reddysahinip@gmail.com>
2023-10-02 01:56:44 +0530

    start with python

---
+++ lsbsteg.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+oimg=cv.imread("owl.bmp")
+cv.imshow("oops",oimg)
+#read first few pixels
+#get string
+#convert asci to binary
+#split bitwise and set image bits
+#display moded image
+#input image
+#store last bits of image
+#convert to ascii
+
+
+
+#how to know when the message ends??
+#display space available
+#possible security login
+
+
+
+cv.waitKey(0)
