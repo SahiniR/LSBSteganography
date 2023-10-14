d8b410b5f984b1d81d6384db9520a67c94defbdd
SahiniR <reddysahinip@gmail.com>
2023-10-15 01:26:33 +0530

    string operations

--- lsbsteg.py
+++ lsbsteg.py
@@ -1,10 +1,40 @@
 import cv2 as cv
-oimg=cv.imread("owl.bmp")
-cv.imshow("oops",oimg)
-#read first few pixels
-#get string
+import numpy as np
+oimg=cv.imread("owl.jpeg")
+
+#reading image & displaying original image
+#cv.imshow("oops",oimg)
+# cv.waitKey(0)
+
+
+# splitting into bgr mat
+b,g,r=cv.split(oimg)
+
+#sring to binary
+str_in=input("input string")
+str_in_bin = ''.join(format(ord(i), '08b') for i in str_in)
+
+
+
+#binary to string
+str_out =' '
+
+for i in range(0, len(str_in_bin), 8):
+
+    temp_data = str_in_bin[i:i + 8]
+    decimal_data = int(temp_data,2)
+    str_out = str_out + chr(decimal_data) 
+ 
+print("The Binary value after string conversion is:",
+       str_out)
+
+
+
+
 #convert asci to binary
-#split bitwise and set image bits
+#split bitwise
+
+#set image bits
 #display moded image
 #input image
 #store last bits of image
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+for i in range(0,col):
+    for j in range(0,rows):
+        mat[j][i]=oimg[j][i]
